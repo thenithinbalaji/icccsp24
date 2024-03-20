2192efaad213dba7738ed6de55ed22ffe9952ca5
thenithinbalaji <thenithinbalaji@gmail.com>
2024-03-20 17:06:37 +0530

    removed speakers

--- components/Speakers/constants.js
+++ components/Speakers/constants.js
@@ -1,51 +1,47 @@
 export const SPEAKERS = [
   {
-    image:
-      'https://scholar.googleusercontent.com/citations?view_op=view_photo&user=TLa4phUAAAAJ&citpid=1',
+    image: '/KeynoteSpeakers/khairunizam.jpg',
     name: 'Prof. Ts. Ir. Dr. Wan Khairunizam',
-    designation:
-      'Brain Machine Interface Research Group (FTKE), Universiti Malaysia Perlis',
+    designation: 'Brain Machine Interface Research Group (FTKE), Universiti Malaysia Perlis',
     description: 'Malaysia',
     profile: 'https://scholar.google.com.my/citations?user=TLa4phUAAAAJ&hl=en',
   },
   {
-    name: 'Vijay John',
-    description: 'RIKEN Institute, Japan',
-    image: 'https://icccsp.com/img/vijay_john.png',
-    designation: 'Research Scientist',
-    profile: 'https://www.linkedin.com/in/vijayjohn1983/?originalSubdomain=jp',
+    image: '/KeynoteSpeakers/vijayjohn.jpg',
+    name: 'Dr. Vijay John',
+    designation: 'Research Scientist, RIKEN Institute',
+    description: 'Japan',
+    profile: 'https://scholar.google.co.jp/citations?user=Wv71RXYAAAAJ&hl=en',
   },
   {
-    name: 'Anand Ponnusamy',
-    description: '',
-    image: '/anandponnusamy.jpg',
-    designation: 'Director-Power Platform/Dynamics 365 at Microsoft',
+    image: '/KeynoteSpeakers/anandponnusamy.jpg',
+    name: 'Mr. Anand Ponnusamy',
+    designation: 'Director-Power Platform/Dynamics 365, Microsoft',
+    description: 'India',
     profile: 'https://www.linkedin.com/in/anandponnusamy/',
   },
   {
-    image:
-      'https://irispublic.universityofgalway.ie/people/images/karlmason.jpg',
+    image: '/KeynoteSpeakers/karlmason.jpg',
     name: 'Dr. Karl Mason',
-    designation:
-      'Lecturer Above the Bar, School of Computer Science, University of Galway',
+    designation: 'Lecturer Above the Bar, School of Computer Science, University of Galway',
     description: 'Galway, Ireland',
     profile: 'https://scholar.google.com/citations?user=kxcJG50AAAAJ&hl=en',
   },
-  {
-    image:
-      'https://www.ieeetoronto.ca/wp-content/uploads/2020/06/22281586_10159537726185595_1945439871642124433_n.jpg',
-    name: 'Dr. Fatima Hussain',
-    designation: 'Adjunct Professor, Toronto Metropolitan University',
-    description: 'Toronto, Canada',
-    profile: 'https://scholar.google.ca/citations?user=87Kzz0wAAAAJ&hl=en',
-  },
+  // {
+  //   image:
+  //     'https://www.ieeetoronto.ca/wp-content/uploads/2020/06/22281586_10159537726185595_1945439871642124433_n.jpg',
+  //   name: 'Dr. Fatima Hussain',
+  //   designation: 'Adjunct Professor, Toronto Metropolitan University',
+  //   description: 'Toronto, Canada',
+  //   profile: 'https://scholar.google.ca/citations?user=87Kzz0wAAAAJ&hl=en',
+  // },
 
-  {
-    image:
-      'https://media.licdn.com/dms/image/C4E03AQFZXltRBCABeA/profile-displayphoto-shrink_800_800/0/1572184398132?e=2147483647&v=beta&t=51LOQAio1QOiz9NJQzfOe-NIUttkt3cQ5NraRt1AigA',
-    name: 'Mr. Shankar Ramasubramanian',
-    designation: 'Competency Head - Big Data at Renault-Nissan-Mitsubishi',
-    description: 'Chennai, India',
-    profile: 'https://in.linkedin.com/in/shankarthebest',
-  },
+  // {
+  //   image:
+  //     'https://media.licdn.com/dms/image/C4E03AQFZXltRBCABeA/profile-displayphoto-shrink_800_800/0/1572184398132?e=2147483647&v=beta&t=51LOQAio1QOiz9NJQzfOe-NIUttkt3cQ5NraRt1AigA',
+  //   name: 'Mr. Shankar Ramasubramanian',
+  //   designation: 'Competency Head - Big Data at Renault-Nissan-Mitsubishi',
+  //   description: 'Chennai, India',
+  //   profile: 'https://in.linkedin.com/in/shankarthebest',
+  // },
 ];
